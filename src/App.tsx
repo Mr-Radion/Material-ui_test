9ed2b1e155fca8cc9494654818f600939eeabf3b
Mr.Radion <mr.rodion_oa@mail.ru>
2021-01-01 21:54:21 +0300

    create top menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  IconButton,
+  makeStyles,
+  Typography,
+  Box,
+  Button,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+
+// useStyles is a function for generating classes
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2), // 1 = 8 px by default
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="fixed">
+        <Container fixed>
+          <Toolbar>
+            {' '}
+            {/*put buttons, links, pictures for a responsive menu here */}
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              News
+            </Typography>
+            <Box mr={3}>
+              <Button color="inherit" variant="outlined">
+                Log in
+              </Button>
+            </Box>
+            <Button color="secondary" variant="contained">
+              Sign up
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
     </div>
   );
 }
